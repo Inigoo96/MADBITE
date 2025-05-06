e082edcab2ffeefc8bf825a7a033bad1d52ba2c4
inigoo96 <inigoal96@gmail.com>
2025-05-06 18:18:25 +0200

    Actualizacion back cognito

--- src/main/java/org/madbite/controller/CognitoCallbackServlet.java
+++ src/main/java/org/madbite/controller/CognitoCallbackServlet.java
@@ -32,8 +32,7 @@ public class CognitoCallbackServlet extends HttpServlet {
     private static final String CLIENT_ID     = "6omlacb6fjdnimu8dalu81ft0r";
     private static final String CLIENT_SECRET = "r3a3i2crbscfmbfr01v36s6mtajc036lohr4ibq4c8ui9918qfp";   // ← si tu app client NO lleva secret, déjalo vacío
     private static final String REDIRECT_URI  = "http://localhost:3007/html/menu";
-    private static final String TOKEN_ENDPOINT =
-            "https://us-east-19ns1g8vpk.auth.us-east-1.amazoncognito.com/oauth2/token";
+    private static final String TOKEN_ENDPOINT = "https://us-east-19ns1g8vpk.auth.us-east-1.amazoncognito.com/oauth2/token";
     /* ─────────────────────────────────────────────────────────── */
 
     private final ObjectMapper mapper  = new ObjectMapper();
